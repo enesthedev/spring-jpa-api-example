af973c21d03e680beb7531869f396eba79f38063
Enes Bayraktar <enes.bayraktar@outlook.com.tr>
2023-01-12 09:52:16 +0300

    Add: Customer entity

---
+++ src/main/java/com/enesthedev/customer/entities/Customer.java
@@ -0,0 +1,29 @@
+package com.enesthedev.customer.entities;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Getter
+@Setter
+@Entity
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "firstName")
+    private String firstName;
+
+    @Column(name = "lastName")
+    private String lastName;
+
+    @Column(name = "telephoneNumber")
+    private String telephoneNumber;
+
+    @Column(name = "birthDate")
+    private Date birthDate;
+}
